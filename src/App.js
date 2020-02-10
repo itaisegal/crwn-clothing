3fc3242941c13190baa1fe0ea82d1404168ddeee
איתי סגל <itaise@clalit.org.il>
2020-02-10 11:51:30 +0200

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.components';
 import ShopPgae from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
-    <div>
+		<div>
+			<Header/>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
 				<Route path='/shop' component={ShopPgae} />
