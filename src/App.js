733990eab7476334d83506f806e0e4011c72d2c2
איתי סגל <itaise@clalit.org.il>
2020-02-10 11:25:07 +0200

    created shop preview and items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.components';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.components';
+import ShopPgae from './pages/shop/shop.component';
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/shop' component={ShopPgae} />
+			</Switch>
     </div>
   );
 }
