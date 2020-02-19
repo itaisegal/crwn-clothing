9a09f27e3f8138281f9e68ee7a98b2cd8dd88b43
איתי סגל <itaise@clalit.org.il>
2020-02-19 11:28:38 +0200

    added redux and userReducer and userActions to the heade and app components

--- src/App.js
+++ src/App.js
@@ -2,38 +2,34 @@ import React from 'react';
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import HomePage from './pages/homepage/homepage.components';
 import ShopPgae from './pages/shop/shop.component';
 import SignupLogin from './pages/signup-login/signup-login.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
-					});
+          });
         });
-        
       } else {
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
     });
   }
@@ -45,7 +41,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPgae} />
@@ -56,4 +52,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapStateToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapStateToProps)(App);
