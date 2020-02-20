fdc35afda778e62c64c1026fdf693c5b83943ad1
איתי סגל <itaise@clalit.org.il>
2020-02-20 09:51:42 +0200

    create cart-item component and connected cart with cartitem reducer

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
 import './cart.styles.scss';
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
   <div className='cart'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={CartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECK OUT</CustomButton>
   </div>
 );
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(Cart);
