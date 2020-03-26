f59effaf9eac5f34e9df2af98162792fe665505a
Itai Segal <itai.segal@gmail.com>
2020-03-26 14:22:26 +0200

    adding thunk and using it in shop and collections fetching

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,32 @@
 import ShopActionsTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionsTypes.UPDATE_COLLETIONS,
+import {
+  firestore,
+  convertCollectionSnapshopToMap
+} from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = collectionsMap => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+
+export const fetchCollectionsFailure = errorMessage => ({
+	type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
+	payload: errorMessage
+})
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+
+    collectionRef.get().then(snapShot => {
+      const collectionsMap = convertCollectionSnapshopToMap(snapShot);
+      dispatch(fetchCollectionsSuccess(collectionsMap));
+    }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
